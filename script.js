c134157dd347f00e9f6889b2fb9f5417760d8b9b
Mutuma Gitonga <mgitonga150@gmail.com>
2022-09-03 11:45:31 +0300

    add geocoding api function to get lat & lon for queriedCity and throw errors for non-existent cities

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
+const getQueriedCityGpsCoordinates = (queriedCity) => {
+
+  // Pass the queriedCity into the request URL
+  fetch(`https://api.api-ninjas.com/v1/geocoding?city=${queriedCity}`, 
+  {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': 'n0CwaZayigy3k6UpI8NF/g==41LYGmWHvAVCyauA',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      // Check if HTTP response successful (res.ok)
+      // res.ok returns true for success codes (200-299)
+      // Throws errors on unsuccessful requests 400/500 status codes, whose status we would otherwise never know
+      if(!res.ok) {
+        console.log(res);
+        throw new Error(`Error! Status: ${res.status}`);
+      }
+      
+      // NB: With Network errors, the .status property is never populated (since error ain't HTTP response) 
+      // In addition, CORS error/misspelt URLs errors are caught by the .CATCH block chained to the fetch promise
+      
+      return res.json();
+    })
+      .then(coordinates => {
+
+        // Check if city name is available or valid before extracting its coordinates
+        // That is, the object/array is non-empty
+        if(coordinates.length !== 0) {
+          // Extracting city latitude and longitude from object
+          // These coordinates are extracted from the first array entry as it's most relevant (internationally renowned)
+          let lat = coordinates[0]["latitude"];
+          let lon = coordinates[0]["longitude"];;
+        }
+          // Otherwise throw an error that city not found
+          else {
+            // throw new Error("City not found! Check spelling or try another.");
+            throw new Error ("City not found in database! Check spelling or try another one.");
+          }
+        
+        // Pass geo coordinates to weather info function
+        // getQueriedCityWeatherInformation(lat,lon);
+        console.log(coordinates);
+      })
+        .catch(err => {
+          // Pop up the thrown error message alert
+          alert(err);
+        });
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
   // Select form object
   const cityQueryForm = document.querySelector('#cityQuery');
-  
+
   // Listen for submit event & store typed value
   cityQueryForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -18,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
       
         // Trim queriedCity of any white spaces on both sides then use REGEX to affirm it ONLY HAS ALPHA CHARACTERS
         if(!/[^a-zA-Z]/.test(queriedCity.trim())) { 
-          //If warning exists, remove it before adding another 
+          //If warning exists, remove it since name is now valid 
           if(formHasWarningParagraph())
             removeWarningParagraph();
           
@@ -27,6 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
           // Input string valid, call the get gps coordinates function, passing in queriedCity parameter
           getQueriedCityGpsCoordinates(queriedCity); 
+          
         } 
           // IF NON-ALPHA CHARS FOUND:
           else {
@@ -41,7 +95,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Append warning first time
                 appendNonAlphaCharsWarning();
               }
-
           }
     } 
     // ELSE (EMPTY FIELD SUBMITTED)
@@ -96,9 +149,9 @@ document.addEventListener('DOMContentLoaded', () => {
     p.className = "form#cityQuery p";
 
     cityQueryForm.appendChild(p);
-    queriedCity = '';
   }
   // *************************************************
   // ******* INPUT VALIDATION FUNCTIONS END **********
   // *************************************************
+
 });
